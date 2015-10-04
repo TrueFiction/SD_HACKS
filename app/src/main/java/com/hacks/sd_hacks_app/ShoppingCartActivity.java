120f73cc1449e7b44ac74d504844123c690013ed
jsenar <JLSenar@gmail.com>
2015-10-03 20:14:23 -0700

    Shopping cart activity

---
+++ app/src/main/java/com/hacks/sd_hacks_app/ShoppingCartActivity.java
@@ -0,0 +1,13 @@
+package com.hacks.sd_hacks_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShoppingCartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
